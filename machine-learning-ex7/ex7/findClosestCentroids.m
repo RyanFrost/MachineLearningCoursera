2cc04534217c98c82183c8aef1823083964bb9d4
RyanDesktopLinux <ryanwindsorfrost@gmail.com>
2015-05-24 16:09:23 -0700

    ex7 complete

---
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -0,0 +1,37 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every example, find its closest centroid, and store
+%               the index inside idx at the appropriate location.
+%               Concretely, idx(i) should contain the index of the centroid
+%               closest to example i. Hence, it should be a value in the 
+%               range 1..K
+%
+% Note: You can use a for-loop over the examples to compute this.
+%
+
+% pdist2 finds the euclidean distance from each X point to each of the
+% three centroids min finds the minimum distance for each X point. Then,
+% idx is the index of that minimum distance for each X point (which gives
+% the index of the centroid to which each X point is assigned
+
+[~,idx] = min(pdist2(centroids,X,'euclidean'));
+
+
+
+
+% =============================================================
+
+end
+
